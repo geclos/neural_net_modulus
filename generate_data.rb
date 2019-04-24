ff8cf93f2e0b625e28da4ae1b005b7de74a3ac81
Gerard Clos <clos.gerard@gmail.com>
2019-04-24 17:37:45 +0200

    🎉 first commit

---
+++ generate_data.rb
@@ -0,0 +1,11 @@
+file = File.new("data.csv",  "w+")
+
+(0..10000).each do |_|
+  number = ( '%08d' % (Random.rand(99999999)) ).to_i
+  letter = 'TRWAGMYFPDXBNJZSQVHLCKE'[number % 23]
+
+  file << "#{number},#{letter}\n"
+  putc '.'
+end
+
+file.close
