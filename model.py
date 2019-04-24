7517b4415d75e7281964a8e57656da640101c53e
Gerard Clos <clos.gerard@gmail.com>
2019-04-24 18:55:20 +0200

    second commit

--- model.py
+++ model.py
@@ -1,4 +1,17 @@
+import csv
 import numpy as np
-import tensforflow as tf
+import tensorflow as tf
 
+reader = csv.reader(open("data.csv", "rb"), delimiter=",")
+arr = list(reader)
+result = np.array(arr)
+
+X = map(lambda x: x[0], result)
+Y = map(lambda x: x[1], result)
+
+X_train, X_test = tf.split(X, [8999, 1000])
+Y_train, Y_test = tf.split(Y, [8999, 1000])
+
+tf.shape(X_train)
+tf.shape(Y_train)
 
