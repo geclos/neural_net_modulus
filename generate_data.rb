a16f7f2ce1cbecc36d95c93b44cfa46604e85da9
Gerard Clos <clos.gerard@gmail.com>
2019-04-28 12:32:53 +0200

    Compiled NN

--- generate_data.rb
+++ generate_data.rb
@@ -1,8 +1,8 @@
 file = File.new("data.csv",  "w+")
 
 (0..10000).each do |_|
-  number = ( '%08d' % (Random.rand(99999999)) ).to_i
-  letter = 'TRWAGMYFPDXBNJZSQVHLCKE'[number % 23]
+  number = ( '%08d' % (Random.rand(99999999).to_i) )
+  letter = 'TRWAGMYFPDXBNJZSQVHLCKE'[number.to_i % 23]
 
   file << "#{number},#{letter}\n"
   putc '.'
--- model.py
+++ model.py
@@ -1,17 +1,227 @@
+import pdb
+import re
 import csv
+import math
 import numpy as np
 import tensorflow as tf
 
-reader = csv.reader(open("data.csv", "rb"), delimiter=",")
-arr = list(reader)
-result = np.array(arr)
+def to_int(arr):
+    return list(map(lambda val: int(val), arr))
 
-X = map(lambda x: x[0], result)
-Y = map(lambda x: x[1], result)
+def to_zeros(i):
+    zeros = np.zeros(23)
+    zeros[i] = 1
+    return zeros
 
-X_train, X_test = tf.split(X, [8999, 1000])
-Y_train, Y_test = tf.split(Y, [8999, 1000])
+def convert_labels_to_integers(labels):
+    labels_dict = {
+        'T': 0,
+        'R': 1,
+        'W': 2,
+        'A': 3,
+        'G': 4,
+        'M': 5,
+        'Y': 6,
+        'F': 7,
+        'P': 8,
+        'D': 9,
+        'X': 10,
+        'B': 11,
+        'N': 12,
+        'J': 13,
+        'Z': 14,
+        'S': 15,
+        'Q': 16,
+        'V': 17,
+        'H': 18,
+        'L': 19,
+        'C': 20,
+        'K': 21,
+        'E': 22
+    }
 
-tf.shape(X_train)
-tf.shape(Y_train)
+    return list(map(lambda x: to_zeros(labels_dict[x]), labels))
 
+# INITIALIZE PLACEHOLDERS
+def create_placeholders(n_x, n_y):
+    X = tf.compat.v1.placeholder(tf.float32, shape = (n_x, None), name= 'X')
+    Y = tf.compat.v1.placeholder(tf.float32, shape = (n_y, None), name= 'Y')
+
+    return X, Y
+
+# INITIALIZE PARAMATERS
+def initialize_parameters():
+    W1 = tf.compat.v1.get_variable("W1", [23,8], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b1 = tf.compat.v1.get_variable("b1", [23,1], initializer = tf.zeros_initializer())
+
+    parameters = { "W1": W1, "b1": b1 }
+
+    return parameters
+
+# FORWARD PROPAGATION
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+
+    Z1 = tf.add(tf.matmul(W1, X), b1)
+
+    return Z1
+
+# COMPUTE COST
+def compute_cost(Z1, Y):
+    logits = tf.transpose(Z1)
+    labels = tf.transpose(Y)
+
+    print(labels.shape, logits.shape)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+
+    return cost
+
+# CREATE RANDOM MINI BATCHES
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+    """
+    Creates a list of random minibatches from (X, Y)
+
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    mini_batch_size - size of the mini-batches, integer
+    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
+
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+
+    m = X.shape[1]                  # number of training examples
+    mini_batches = []
+    np.random.seed(seed)
+
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((Y.shape[0],m))
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+# THE MODEL
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
+          num_epochs = 1500, minibatch_size = 32, print_cost = True):
+    """
+    Implements a three-layer tensorflow neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SOFTMAX.
+
+    Arguments:
+    X_train -- training set, of shape (input size = 12288, number of training examples = 1080)
+    Y_train -- test set, of shape (output size = 6, number of training examples = 1080)
+    X_test -- training set, of shape (input size = 12288, number of training examples = 120)
+    Y_test -- test set, of shape (output size = 6, number of test examples = 120)
+    learning_rate -- learning rate of the optimization
+    num_epochs -- number of epochs of the optimization loop
+    minibatch_size -- size of a minibatch
+    print_cost -- True to print the cost every 100 epochs
+
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    tf.set_random_seed(1)                             # to keep consistent results
+    seed = 3                                          # to keep consistent results
+    (n_x, m) = X_train.shape                          # (n_x: input size, m : number of examples in the train set)
+    n_y = Y_train.shape[0]                            # n_y : output size
+    costs = []                                        # To keep track of the cost
+
+    # Create Placeholders of shape (n_x, n_y)
+    X, Y = create_placeholders(n_x, n_y)
+
+    # Initialize parameters
+    parameters = initialize_parameters()
+
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    Z3 = forward_propagation(X, parameters)
+
+    # Cost function: Add cost function to tensorflow graph
+    cost = compute_cost(Z3, Y)
+
+    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer.
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+
+    # Initialize all the variables
+    init = tf.global_variables_initializer()
+
+    # Start the session to compute the tensorflow graph
+    with tf.Session() as sess:
+
+        # Run the initialization
+        sess.run(init)
+
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            epoch_cost = 0.                       # Defines a cost related to an epoch
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the "optimizer" and the "cost", the feedict should contain a minibatch for (X,Y).
+                _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+
+                epoch_cost += minibatch_cost / num_minibatches
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 100 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, epoch_cost))
+            if print_cost == True and epoch % 5 == 0:
+                costs.append(epoch_cost)
+
+        # lets save the parameters in a variable
+        parameters = sess.run(parameters)
+        print ("Parameters have been trained!")
+
+        # Calculate the correct predictions
+        correct_prediction = tf.equal(tf.argmax(Z3), tf.argmax(Y))
+
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print ("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
+        print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+
+        return parameters
+
+# Import data
+reader = csv.reader(open("data.csv", "r"), delimiter=",")
+result = np.array(list(reader))
+
+# Refactor data
+X = np.array(list(map(lambda x: to_int(re.findall(r'\d', x[0])), result)))
+Y = np.array(convert_labels_to_integers(result[:,1]))
+
+pdb.set_trace()
+
+# Extract train and test sets
+X_train = X[:9000].reshape((8, 9000))
+X_test = X[9001:].reshape((8, 1000))
+Y_train = Y[:9000].reshape((23, 9000))
+Y_test = Y[9001:].reshape((23, 1000))
+
+model(X_train, Y_train, X_test, Y_test)
